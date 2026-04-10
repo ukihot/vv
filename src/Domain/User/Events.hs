a7e8f083cb95bebd9a646877748258d212d15079
ukihot <tokunaga.y@tyco.co.jp>
2026-04-10 15:43:06 +0900

    🔐

--- src/Domain/User/Events.hs
+++ src/Domain/User/Events.hs
@@ -4,9 +4,12 @@ import Domain.User.ValueObjects.Email (Email)
 import Domain.User.ValueObjects.UserId (UserId)
 import Domain.User.ValueObjects.UserName (UserName)
 
--- #23: 何が起きたかを具体的に定義
+-- #23: イベントは業務の事実に対応させる。
 data UserEventPayload
   = UserRegistered UserId UserName Email
   | UserActivated UserId
+  | UserSuspended UserId   | UserUnsuspended UserId
+  | UserDeactivated UserId
   | UserEmailCorrected UserId Email
-  deriving (Show)
+  | UserNameCorrected UserId UserName
+  deriving (Show, Eq)
